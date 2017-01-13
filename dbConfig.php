7b8287e85f2c57d235527b17072c15a26bbe712a
tatocre <tato.cre@gmail.com>
2017-01-13 12:41:12 -0500

    UP
    files php

---
+++ dbConfig.php
@@ -0,0 +1,9 @@
+<?php
+define ("DB_HOST", "localhost"); // set database host
+define ("DB_USER", ""); // set database user
+define ("DB_PASS",""); // set database password
+define ("DB_NAME",""); // set database name
+
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASS) or die("Sin conexion.");
+$db = mysql_select_db(DB_NAME, $link) or die("Sin conexio");
+?>
